b3f64a420fbda64d9858781ce3f60006a64102bd
Max-Bos <9021550@student.zadkine.nl>
2024-05-22 14:55:37 +0200

    OOP Module 1

---
+++ Product.php
@@ -0,0 +1,14 @@
+<?php
+
+class product
+{
+
+}
+
+$Playstation5 = new product();
+$XboxSeriesX = new product();
+$NintendoSwitch = new product();
+
+var_dump($Playstation5);
+var_dump($XboxSeriesX);
+var_dump($NintendoSwitch);
