9c1886a262c1309152638e13fe5191bd96d48ebc
Max-Bos <9021550@student.zadkine.nl>
2024-06-05 15:50:33 +0200

    Opdracht 2

--- Product.php
+++ Product.php
@@ -2,13 +2,20 @@
 
 class product
 {
-
+ public $name = "Product";
 }
 
 $Playstation5 = new product();
+$Playstation5->name = "Playstation5";
+
 $XboxSeriesX = new product();
+$XboxSeriesX->name = "XboxSeriesX";
+
 $NintendoSwitch = new product();
 
 var_dump($Playstation5);
 var_dump($XboxSeriesX);
-var_dump($NintendoSwitch);
+
+echo $Playstation5->name . "<br>"	;
+echo $XboxSeriesX->name . "<br>"	;
+echo $NintendoSwitch->name . "<br>"	;
