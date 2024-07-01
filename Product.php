514a3a589719ba1c986fb3cceb8b3138db68dfea
Max-Bos <9021550@student.zadkine.nl>
2024-07-01 11:41:05 +0200

    tot opdracht 8

--- Product.php
+++ Product.php
@@ -1,40 +1,34 @@
 <?php
 
-class product
+class Product
 {
- public $name;
- public $price;
- public $currency;
+  public $name;
+  public $price;
+  public $currency;
+  public function __construct($price, $name, $currency = "&euro")
+  {
+    $this->name = ucfirst($name);
+    $this->price = $price;
+    $this->currency = $currency;
+  }
 
- public function __construct($name, $price, $currency = "€")
- {
-  $this->name = ucfirst($name);
-  $this->price = $price;
-  $this->currency = $currency;
- }
-
- public function formatPrice()
- {
-  return $this->currency . number_format($this->price, 2);
- }
+  public function formatPrice()
+  {
+    return number_format($this->price, decimals:2);
+  }
+  public function getProduct()
+  {
+    return "Het product" .$this->name."kost" .$this->currency."" .$this->price;
+  }
 }
 
-$game1 = new product(name: "Hollow Knight", price: 14.99, currency: "$");
+$game1 = new Product(price:14.99, name:"Hollow Knight", currency:"$");
+echo $game1->getProduct();
 
 
-$game2 = new product("God of War", 19.99);
 
-
-$game3 = new product("The Last of Us", 24.99);
-
-echo $game1->name . "<br>";
-echo $game2->name . "<br>";
-echo $game3->name . "<br>";
-
-echo $game1->formatPrice() . "<br>";
-echo $game2->formatPrice() . "<br>";
-echo $game3->formatPrice() . "<br>";
+// echo $game1->name . "<br>";
+// echo $game1->currency;
+// echo $game1->formatPrice() . "<br>";
 
 var_dump($game1);
-var_dump($game2);
-var_dump($game3);
