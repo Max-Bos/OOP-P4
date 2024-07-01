4efb17e7e4f8e871742616116fb41dcaabbe539c
Max-Bos <9021550@student.zadkine.nl>
2024-07-01 11:11:26 +0200

    tot opdracht 6

--- Product.php
+++ Product.php
@@ -2,20 +2,39 @@
 
 class product
 {
- public $name = "Product";
+ public $name;
+ public $price;
+ public $currency;
+
+ public function __construct($name, $price, $currency = "€")
+ {
+  $this->name = ucfirst($name);
+  $this->price = $price;
+  $this->currency = $currency;
+ }
+
+ public function formatPrice()
+ {
+  return $this->currency . number_format($this->price, 2);
+ }
 }
 
-$Playstation5 = new product();
-$Playstation5->name = "Playstation5";
+$game1 = new product(name: "Hollow Knight", price: 14.99, currency: "$");
 
-$XboxSeriesX = new product();
-$XboxSeriesX->name = "XboxSeriesX";
 
-$NintendoSwitch = new product();
+$game2 = new product("God of War", 19.99);
 
-var_dump($Playstation5);
-var_dump($XboxSeriesX);
 
-echo $Playstation5->name . "<br>"	;
-echo $XboxSeriesX->name . "<br>"	;
-echo $NintendoSwitch->name . "<br>"	;
+$game3 = new product("The Last of Us", 24.99);
+
+echo $game1->name . "<br>";
+echo $game2->name . "<br>";
+echo $game3->name . "<br>";
+
+echo $game1->formatPrice() . "<br>";
+echo $game2->formatPrice() . "<br>";
+echo $game3->formatPrice() . "<br>";
+
+var_dump($game1);
+var_dump($game2);
+var_dump($game3);
